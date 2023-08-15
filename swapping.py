87158e12fa37e9001e190c400d535d5d3ee6d9d9
shrutipatil2004 <141646253+shrutipatil2004@users.noreply.github.com>
2023-08-15 19:31:46 +0530

    Create swapping.py
    swapping of two numbers with out using third variable

---
+++ swapping.py
@@ -0,0 +1,11 @@
+print("               * Swapping *")
+print("\n")
+a=int(input("* Enter num1:"))
+b=int(input("* Enter num2:"))
+a=a+b
+b=a-b
+a=a-b
+print("\n")
+
+print("* after swaping:",a)
+print("* after swaping:",b)
