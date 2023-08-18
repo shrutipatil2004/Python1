a93e2cf3703bed6e6ad13e6cc35cd82e218b38ff
shrutipatil2004 <141646253+shrutipatil2004@users.noreply.github.com>
2023-08-18 19:23:22 +0530

    Create greater no.py
    Program to find greatest number between three numbers.

---
+++ greater no.py
@@ -0,0 +1,12 @@
+print("              * greater no. *   ")
+print("\n")
+a=int(input("a:"))
+b=int(input("b:"))
+c=int(input("c:"))
+print("\n")
+if a>b and a>c:
+ print("a is greater number")
+elif b>a and b>c:
+ print("b is greater number")
+else:
+ print("c is greater number")
